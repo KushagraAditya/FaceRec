5b2aab9a7cddf827b8cbe2720f9f9dabc64c1a8e
Kushagra Aditya <akushagra641@gmail.com>
2023-08-30 00:27:42 +0530

    embedding in salesforce step1

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template, Response,  request, redirect, url_for
+from flask import Flask, render_template, Response, request, redirect, url_for
 from simple_salesforce import Salesforce
 import cv2
 import face_recognition
 import numpy as np
 from flask import session
 import pickle
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "https://your-salesforce-domain.com"}})
 app.config['SECRET_KEY'] = 'mysecretkey' 
 
 def generate_frame():
@@ -84,6 +86,7 @@ def success():
 @app.route('/failure')
 def failure():
     return "Login Failed!"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
